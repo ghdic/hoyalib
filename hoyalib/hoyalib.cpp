52963fbdd764f842fe9d3e3a16330b15586814c6
ghdic77 <ghdic77@gmail.com>
2018-02-27 12:27:30 +0900

    first

--- hoyalib/hoyalib.cpp
+++ hoyalib/hoyalib.cpp
@@ -1,4 +1,16 @@
-/*
+﻿/*
 This library reference by standard library
 
 */
+
+#include <stdio.h>
+#include <conio.h>
+
+//need include conio.h
+//키보드 눌렸을때 눌린 문자 반환; 아닐경우 0반환(if문조건문에 넣어서 활용)
+// if(choice=get_kbhit()){}
+char get_kbhit() {
+	if (_kbhit)
+		return _getch();
+	return 0;
+}
