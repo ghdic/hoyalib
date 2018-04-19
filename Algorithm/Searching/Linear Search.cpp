cae9f4f13e8ac89f0261794d1d57ac8a33373be4
ghdic <ghdic@naver.com>
2018-04-20 01:52:35 +0900

    Can't understand Cartesian Tree sort, linear search clear

---
+++ Algorithm/Searching/Linear Search.cpp
@@ -0,0 +1,24 @@
+////////////////////////////////
+//	Linear Search
+//	¼øÂ÷ Å½»ö
+////////////////////////////////
+
+/*
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int x) {
+	int i;
+	for (i = 0; i < n; i++)
+		if (arr[i] == x)
+			return i;
+	return -1;
+}
+
+int main() {
+	int arr[] = { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
+	int find = linearSearch(arr, 10, 30);
+	if (find == -1)cout << "Element not found" << endl;
+	else cout << "Element found!" << endl;
+}
+*/
--- test/test.cpp
+++ test/test.cpp
@@ -1,63 +1,13 @@
-#include "Turboc.h"
+#include <cstdio>
+#include <iostream>
+using namespace std;
 
-class Date;
+int main() {
+	int t;
 
-class Time
+	cin >> t;
+	fflush(stdout);
+	for (int i = 0; i < t; i++) {
 
-{
-
-	friend void OutToday(Date &, Time &);
-
-private:
-
-	int hour, min, sec;
-
-public:
-
-	Time(int h, int m, int s) { hour = h; min = m; sec = s; }
-
-};
-
-
-
-class Date
-
-{
-
-	friend void OutToday(Date &, Time &);
-
-private:
-
-	int year, month, day;
-
-public:
-
-	Date(int y, int m, int d) { year = y; month = m; day = d; }
-
-};
-
-
-
-void OutToday(Date &d, Time &t)
-
-{
-
-	printf("오늘은 %d년 %d월 %d일이며 지금 시간은 %d:%d:%d입니다.\n",
-
-		d.year, d.month, d.day, t.hour, t.min, t.sec);
-
-}
-
-
-
-void main()
-
-{
-
-	Date D(2005, 01, 02);
-
-	Time T(12, 34, 56);
-
-	OutToday(D, T);
-
+	}
 }
