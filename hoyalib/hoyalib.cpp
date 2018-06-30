8f91f308b5d9bc06f50020adcd4e238b68f20508
ghdic <ghdic@naver.com>
2018-06-30 21:25:02 +0900

    binary, jump search

--- hoyalib/hoyalib.cpp
+++ hoyalib/hoyalib.cpp
@@ -2,4 +2,54 @@
 This library reference by standard library
 
 */
+#include <string>
+using namespace std;
 
+//자연수인 두 string을 비교한다
+//should include string
+//음수인경우 x
+int compNumStr(string a, string b) {
+	if (a.size() > b.size())return -1;
+	else if (a.size() < b.size())return 1;
+	else {
+		for (int i = a.size() - 1; i >= 0; i--) {
+			if (a[i] > b[i])return -1;
+			else if (a[i] < b[i])return 1;
+		}
+		return 0;
+	}
+}
+
+//자연수인 두 string의 합을 반환한다.
+//should include string
+//음수인경우 x
+string sumNumStr(string a, string b) {
+	string tmp;
+	char add;
+	bool raised = false;
+	int aSize = a.size(), bSize = b.size();
+	int size = aSize > bSize ? aSize : bSize;
+	for (int i = 1; i <= size; i++) {
+
+		if (i > aSize)
+			add = b[bSize - i];
+		else if (i > bSize)
+			add = a[aSize - i];
+		else
+			add = a[aSize - i] + b[bSize - i] - '0';
+
+		if (raised == true)add += 1;
+		if (add - '0' >= 10) {
+			raised = true;
+			add = add - 10;
+			tmp = add + tmp;
+		}
+		else {
+			raised = false;
+			tmp = add + tmp;
+		}
+
+	}
+	if (raised == true)tmp = '1' + tmp;
+	return tmp;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,79 @@
-#include <cstdio>
+/*
+You should use the standard input/output
+
+in order to receive a score properly.
+
+Do not use file input and output
+
+Please be very careful.
+*/
+
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-int main() {
-	int t;
+int Answer;
+vector <vector<int> > ability;
+int main(int argc, char** argv)
+{
+	int T, test_case;
+	/*
+	The freopen function below opens input.txt file in read only mode, and afterward,
+	the program will read from input.txt file instead of standard(keyboard) input.
+	To test your program, you may save input data in input.txt file,
+	and use freopen function to read from the file when using cin function.
+	You may remove the comment symbols(//) in the below statement and use it.
+	Use #include<cstdio> or #include <stdio.h> to use the function in your program.
+	But before submission, you must remove the freopen function or rewrite comment symbols(//).
+	*/
 
-	cin >> t;
-	fflush(stdout);
-	for (int i = 0; i < t; i++) {
+	// freopen("input.txt", "r", stdin);
 
+	cin >> T;
+	for (test_case = 0; test_case < T; test_case++)
+	{
+		int N, K, person, i, j, k;
+		Answer = 0;
+		cin >> N >> K;
+
+		for (i = 0; i < N; i++) {
+			cin >> person;
+			bool check = false;
+			for (j = 0; j < ability.size(); j++) {
+				for (k = 0; k < ability[j].size(); k++) {
+					if (person - K <= ability[j][k] && person + k >= ability[j][k]) {
+						break;
+					}
+					if (k == ability[j].size() - 1) {
+						check = true;
+						ability[j].push_back(person);
+					}
+				}
+				if (check)break;
+			}
+			if (!check) {
+				ability.push_back(vector<int>(1));
+				ability[ability.size()].push_back(person);
+			}
+				
+		}
+		
+		
+		///////////////////////////////////////////////////////////////////////////////
+		/*
+		Implement your algorithm here.
+		The answer to the case will be stored in variable Answer.
+		*/
+		/////////////////////////////////////////////////////////////////////////////////////////////
+
+		// Print the answer to standard output(screen).
+		cout << "Case #" << test_case + 1 << endl;
+		cout << Answer << endl;
+
+		ability.clear();
 	}
+
+	return 0;//Your program should return 0 on normal termination.
 }
